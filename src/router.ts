0688136679dda56ef0ce1200a9acd0d95bb14729
Al-Ghozy03 <muhammadfaizalghozi1@gmail.com>
2024-01-05 07:53:03 +0700

    fix

--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,12 @@ import controller from "./controller";
 
 export const router: Express = express();
 
-router.get("/", controller.root);
 router.get("/complete", controller.complete);
 router.get("/on-going", controller.onGoing);
 router.get("/watch/:href", controller.watch);
 router.get("/genre", controller.genre);
 router.get("/genre/:href", controller.genreDetail);
-router.get("/release-schedule",controller.releaseSchedule)
-router.get("/search",controller.search)
+router.get("/release-schedule", controller.releaseSchedule);
+router.get("/search", controller.search);
 router.get("/:href", controller.detail);
+router.get("/", controller.root);
